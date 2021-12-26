557b0f98fce06accbb8c51d2b2c01e0fe2d6a903
DmodvGH <dmodv@vk.com>
2021-12-26 17:48:21 +0300

    I in home fck

--- detection.py
+++ detection.py
@@ -2,6 +2,8 @@ from imageai.Detection import ObjectDetection
 from numpy.core.numeric import False_
 import os
 
+img="test2"
+
 detector = ObjectDetection()
 
 detector.setModelTypeAsYOLOv3()
@@ -10,10 +12,10 @@ detector.setModelTypeAsYOLOv3()
 detector.setModelPath("yolo.h5")
 detector.loadModel()
 
-custom = detector.CustomObjects(person=False, dog=True)
+custom = detector.CustomObjects(person=True, dog=True)
 
 
-detections = detector.detectCustomObjectsFromImage(custom_objects=custom,input_image="klasss.jpg", output_image_path="imagenew.jpg", minimum_percentage_probability=30)
+detections = detector.detectCustomObjectsFromImage(custom_objects=custom,input_image=(img+".jpg"), output_image_path=(img+"_new.jpg"), minimum_percentage_probability=30)
 for eachObject in detections:
     print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
     print("--------------------------------")
--- ii.py
+++ ii.py
@@ -1,6 +1,8 @@
 from imageai.Classification import ImageClassification
 import os
 
+
+img="test2.jpg"
 execution_path = os.getcwd()
 
 prediction = ImageClassification()
@@ -8,6 +10,6 @@ prediction.setModelTypeAsResNet50()
 prediction.setModelPath(os.path.join(execution_path, "resnet50_imagenet_tf.2.0.h5"))
 prediction.loadModel()
 
-predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "klasss.jpg"), result_count=10)
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, img), result_count=10)
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction , " : " , eachProbability)
