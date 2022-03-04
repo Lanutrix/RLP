2facdb10d5a56c8c2100c22f24ae2c364bf07edf
DmodvGH <dmodv@vk.com>
2022-03-04 21:18:05 +0300

    more add img

--- nepon.py
+++
@@ -1,8 +0,0 @@
-list={
-        "car":3,
-        "person": 5,
-        "dog":9,
-        "cat":10
-    }
-for i in range(len(list)):
-    print(list[i])
--- norm_neironki/load_4.py
+++ norm_neironki/load_4.py
@@ -1,36 +1,49 @@
 import numpy as np
+import cv2 as cv
 import matplotlib.pyplot as plt
 from tensorflow import keras
 from keras.utils import np_utils
 import random, time
 
+def norma(path):
+    x = cv.imread(path)
+    # cv.imshow("pain", x)
+    # cv.waitKey(0)
+    # x = cv.cvtColor(x, cv.COLOR_BGR2GRAY)
+    x=x/255
+    return x
+
+img_input = norma("norm_neironki/car.png")
+
 model = keras.models.load_model('/home/dmodv/git_project/testing/education/main_1_50_eph.h5')
 
 detectionClass=['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+# (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 
-x_train = x_train.astype("float32")
-x_test = x_test.astype("float32")
+# x_train = x_train.astype("float32")
+# x_test = x_test.astype("float32")
 
-x_train = x_train/255
-x_test = x_test/255
+# x_train = x_train/255
+# x_test = x_test/255
 
-y_train = np_utils.to_categorical(y_train)
-y_test  = np_utils.to_categorical(y_test )
+# y_train = np_utils.to_categorical(y_train)
+# y_test  = np_utils.to_categorical(y_test )
 
-for i in range(2):
-    n = random.randint(0,9999)
+# for i in range(2):
+#     n = random.randint(0,9999)
+#     print(np.shape(x_test[n]))
 
-    x = np.expand_dims(x_test[n], axis=0)
+x = img_input.astype("float32")
+x = np.expand_dims(img_input, axis=0)
+print(np.shape(x))
+result = model.predict(x)
 
-    result = model.predict(x)
+predictClass=np.argmax(result)
+plt.imshow(img_input, cmap=plt.cm.binary)
+plt.show()
 
-    predictClass=np.argmax(result)
-    plt.imshow(x_test[n], cmap=plt.cm.binary)
-    plt.show()
+print(f"Class number: {predictClass}\nHis name: {detectionClass[predictClass]}")
 
-    print(f"Class number: {predictClass}\nHis name: {detectionClass[predictClass]}")
-
-    time.sleep(1)
-    i=input()
+time.sleep(1)
+# i=input()
